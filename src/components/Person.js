6e55f34179252137f203f97db64032582dad305b
Vinton Teo <taulago@ymail.com>
2019-06-26 15:35:08 -0600

    Style App, Cohort, and Person components.

--- src/components/Person.js
+++ src/components/Person.js
@@ -5,9 +5,9 @@ const Person = ({ image, name, quote, superlative }) => {
   return (
     <article className='person-card'>
       <img src={image} />
-      <h3 className='name'>{name}</h3>
-      <i className='quote'>{quote}</i>
-      <b className='super'>{superlative}</b>
+      <h3 className='text name'>{name}</h3>
+      <i className='text quote'>{quote}</i>
+      <b className='text super'>{superlative}</b>
     </article>
   )
 }
