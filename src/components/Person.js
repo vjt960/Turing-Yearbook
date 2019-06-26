559da6870a7eabe31deed2ff2ec84d2f49dd06c7
Vinton Teo <taulago@ymail.com>
2019-06-26 14:23:40 -0600

    Create Person component.

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+const Person = ({ image, name, quote, superlative }) => {
+  return (
+    <article className='person-card'>
+      <img src={image} />
+      <h3 className='name'>{name}</h3>
+      <i className='quote'>{quote}</i>
+      <b className='super'>{superlative}</b>
+    </article>
+  )
+}
 
 export default Person;
